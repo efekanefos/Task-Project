862b5a18ba167a1a0d6716295077d6a95fad2940
Efekan EFE <54044105+efekanefos@users.noreply.github.com>
2021-05-21 09:42:00 +0300

    keyandclassnames

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -20,7 +20,6 @@ function Products() {
     fetch("/test/api/properties/", requestOptions)
       .then((response) => response.json())
       .then((result) => {
-        console.log(result);
         setResult(result);
       })
       .catch((error) => console.log("error", error));
@@ -32,48 +31,48 @@ function Products() {
         <div className="row">
           {result.map((item, index) => {
             return (
-              <div className="col-md-3">
-                <div class="card">
+              <div key={index} className="col-md-3">
+                <div className="card">
                   {item.room_type.label === "3 Bed Pool" ? (
-                    <img src={bed3pool} class="card-img-top" alt="..." />
+                    <img src={bed3pool} className="card-img-top" alt="..." />
                   ) : item.room_type.label === "1 Bed Smart" ? (
-                    <img src={bed1smart} class="card-img-top" alt="..." />
+                    <img src={bed1smart} className="card-img-top" alt="..." />
                   ) : item.room_type.label === "1 Bed Apt" ? (
-                    <img src={bed1apt} class="card-img-top" alt="..." />
+                    <img src={bed1apt} className="card-img-top" alt="..." />
                   ) : item.room_type.label === "Studio" ? (
-                    <img src={studio} class="card-img-top" alt="..." />
+                    <img src={studio} className="card-img-top" alt="..." />
                   ) : item.room_type.label === "3 Bed PH" ? (
-                    <img src={bed3ph} class="card-img-top" alt="..." />
+                    <img src={bed3ph} className="card-img-top" alt="..." />
                   ) : item.room_type.label === "3 Bed Apt" ? (
-                    <img src={bed3apt} class="card-img-top" alt="..." />
+                    <img src={bed3apt} className="card-img-top" alt="..." />
                   ) : item.room_type.label === "2 Bed Apt" ? (
-                    <img src={bed2apt} class="card-img-top" alt="..." />
+                    <img src={bed2apt} className="card-img-top" alt="..." />
                   ) : item.room_type.label === "2 Bed PH" ? (
-                    <img src={bed2ph} class="card-img-top" alt="..." />
+                    <img src={bed2ph} className="card-img-top" alt="..." />
                   ) : item.room_type.label === "2 Bed Smart" ? (
-                    <img src={bed2smart} class="card-img-top" alt="..." />
+                    <img src={bed2smart} className="card-img-top" alt="..." />
                   ) : (
                     ""
                   )}
 
-                  <div class="card-body">
-                    <h5 class="card-title">{item.code}</h5>
+                  <div className="card-body">
+                    <h5 className="card-title">{item.code}</h5>
                   </div>
-                  <ul class="list-group list-group-flush">
-                    <li class="list-group-item">
+                  <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
                       FLOOR TYPE {item.room_type.label}
                     </li>
-                    <li class="list-group-item">INTERNAL {item.net_area}</li>
-                    <li class="list-group-item">EXTERNAL {item.out_area}</li>
+                    <li className="list-group-item">
+                      INTERNAL {item.net_area}
+                    </li>
+                    <li className="list-group-item">
+                      EXTERNAL {item.out_area}
+                    </li>
                   </ul>
                 </div>
               </div>
             );
           })}
-
-          <div className="col-md-10">
-            <h1>hello</h1>
-          </div>
         </div>
       </div>
     </div>
